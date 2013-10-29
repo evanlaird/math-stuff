7c7c72f682e8ec4a174181b6082854a4c58d7441
Evan Laird <evan laird@qsecofrs-MacBook-Pro.local>
2013-10-28 21:25:58 -0400

    Temperature check was backwards. Fixed Create and destroy matrices for fun

--- runtime.c
+++ runtime.c
@@ -110,9 +110,11 @@ int main(int argc, char *argv[]) {
     }
 
     // validate the inputs
-    if (start - stop < 0) {
+    if (stop - start < 0) {
         print_usage();
         die("Invalid temperatures");
     }
+    Matrix *lattice = Matrix_create(lattice_size);
+    Matrix_destroy(lattice);
     return 0;
 }
